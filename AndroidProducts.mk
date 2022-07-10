c0f425df8f2b192d9c77544ed03a6e398d635074
sayann <sayansarkarda2@gmail.com>
2022-07-10 06:14:27 +0000

    certus: CipherOS bringup
    Signed-off-by: sayann <sayansarkarda2@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_certus.mk
+    $(LOCAL_DIR)/cipher_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_certus-user \
-    nad_certus-userdebug \
-    nad_certus-eng
+    cipher_certus-user \
+    cipher_certus-userdebug \
+    cipher_certus-eng
